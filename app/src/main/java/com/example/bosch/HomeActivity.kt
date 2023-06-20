1a99b828849f350909cfc2baa67533372745ffcb
Abdul <abdul.tanveer@gmail.com>
2023-06-20 13:26:53 +0300

    passing data b/w activities using intents

--- app/src/main/java/com/example/bosch/HomeActivity.kt
+++ app/src/main/java/com/example/bosch/HomeActivity.kt
@@ -1,13 +1,19 @@
 package com.example.bosch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
-
+    lateinit var homeTv: TextView;
+    //var nameEt: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        homeTv = findViewById(R.id.tvHome);
+       var data:String? =  intent.extras?.getString("hkey")
+        homeTv.text = data
     }
 }
--- app/src/main/java/com/example/bosch/MainActivity.java
+++ app/src/main/java/com/example/bosch/MainActivity.java
@@ -6,19 +6,24 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText nameEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView = inflating xml layout
         setContentView(R.layout.activity_main);
+        nameEt = findViewById(R.id.etName);
     }
 
     public void showToast(View view) {
-        Toast.makeText(this, "welcome to android", Toast.LENGTH_SHORT).show();
-        Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
+        String name = nameEt.getText().toString();
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+       Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
+       hIntent.putExtra("hkey",name);
         startActivity(hIntent);
     }
 
