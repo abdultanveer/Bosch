54490a24075f30b4ae16fe70e83d3a2a7affb74e
Abdul <abdul.tanveer@gmail.com>
2023-06-20 12:04:59 +0300

    onclick launch another activity

--- app/src/main/java/com/example/bosch/MainActivity.java
+++ app/src/main/java/com/example/bosch/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.bosch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, "welcome to android", Toast.LENGTH_SHORT).show();
+        Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
+        startActivity(hIntent);
     }
 }
