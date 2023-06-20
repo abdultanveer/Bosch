207a01c0b9e5056fc0ce62411ab706c9f4a7cd84
Abdul <abdul.tanveer@gmail.com>
2023-06-20 13:00:41 +0300

    intents launch activities of other apps

--- app/src/main/java/com/example/bosch/MainActivity.java
+++ app/src/main/java/com/example/bosch/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bosch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -19,5 +20,11 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "welcome to android", Toast.LENGTH_SHORT).show();
         Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
         startActivity(hIntent);
+    }
+
+    //https://developer.android.com/guide/components/intents-common#java
+    public void openDialer(View view) {
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"));
+        startActivity(dialIntent);
     }
 }
