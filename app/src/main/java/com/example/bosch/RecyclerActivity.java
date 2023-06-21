674fd524cdddfaa7a08d3881224d6f171784f91f
Abdul <abdul.tanveer@gmail.com>
2023-06-21 11:51:14 +0300

    lifecycle of an activity

--- app/src/main/java/com/example/bosch/RecyclerActivity.java
+++ app/src/main/java/com/example/bosch/RecyclerActivity.java
@@ -5,24 +5,69 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class RecyclerActivity extends AppCompatActivity {
-    String[] languagesJson = {"English","hindi","urdu","tamil"};
+    String[] languagesJson = {"English","hindi","urdu","tamil",
+            "English","hindi","urdu","tamil",
+            "English","hindi","urdu","tamil",
+            "English","hindi","urdu","tamil",
+            "English","hindi","urdu","tamil",
+            "English","hindi","urdu","tamil",
+            "English","hindi","urdu","tamil",
+            "English","hindi","urdu","tamil",
+            "English","hindi","urdu","tamil",
+            "English","hindi","urdu","tamil",
+            "English","hindi","urdu","tamil",};
     RecyclerView langRecylerView;
-
+    public static String TAG = RecyclerActivity.class.getSimpleName();
     @Override
 
+    //
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG,"im create");
         setContentView(R.layout.activity_recycler);   //R.layout -- my app
         langRecylerView = findViewById(R.id.langList);
         langRecylerView.setLayoutManager(new LinearLayoutManager(this));
         LangAdapter adapter = new LangAdapter(languagesJson);
         langRecylerView.setAdapter(adapter);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"im started-- visible");
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"im resume - back to the foreground-- restore state");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"im pause - background--store state");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"im stopping - cold storage--- relase resource");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"im destroy - no more");
 
     }
 }
